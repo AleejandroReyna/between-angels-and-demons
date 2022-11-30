adb41965a0a40410f9268548e04494e5f96ca8e8
Alejandro Reyna <aleejandroreyna@gmail.com>
2022-11-29 19:23:30 -0600

    functions-init: Added default header and footer

---
+++ template-parts/footers/main-footer.php
@@ -0,0 +1,8 @@
+<footer class="main-footer">
+
+<?php
+    $menu_args = array('theme_location'=>'footer_menu', 'container' => 'nav');
+    wp_nav_menu($menu_args);
+?>
+
+</footer>
---
+++ template-parts/headers/main-header.php
@@ -0,0 +1,14 @@
+<header class="main-header">
+    <?php
+        if ( has_custom_logo() ) {
+            the_custom_logo();
+        } else { ?>
+            <a href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php echo bloginfo( 'name' );?></a>
+        <?php }
+    ?>
+
+    <?php
+        $menu_args = array( 'theme_location' => 'top_menu' , 'container' => 'nav' );
+        wp_nav_menu( $menu_args );
+    ?>
+</header>
