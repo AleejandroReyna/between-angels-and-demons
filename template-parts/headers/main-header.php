b2805d13fae5f98eb3ff9c70f7ba99bf48dc9ea5
Alejandro Reyna <aleejandroreyna@gmail.com>
2022-11-30 23:43:17 -0600

    theme-styles: Experimenting with colors, fonts and dark mode

--- template-parts/headers/main-header.php
+++ template-parts/headers/main-header.php
@@ -1,14 +1,18 @@
 <header class="main-header">
-    <?php
-        if ( has_custom_logo() ) {
-            the_custom_logo();
-        } else { ?>
-            <a class="blog-title" href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php echo bloginfo( 'name' );?></a>
-        <?php }
-    ?>
+    <div class="header-container">
+        <div class="header-content">
+            <?php
+                if ( has_custom_logo() ) {
+                    the_custom_logo();
+                } else { ?>
+                    <a class="blog-title" href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php echo bloginfo( 'name' );?></a>
+                <?php }
+            ?>
 
-    <?php
-        $menu_args = array( 'theme_location' => 'top_menu' , 'container' => 'nav' );
-        wp_nav_menu( $menu_args );
-    ?>
+            <?php
+                $menu_args = array( 'theme_location' => 'top_menu' , 'container' => 'nav', 'class' => 'header-nav');
+                wp_nav_menu( $menu_args );
+            ?>
+        </div>
+    </div>
 </header>
