fc40a740b4790281dae1840f8544544f04d5a264
Alejandro Reyna <aleejandroreyna@gmail.com>
2023-04-07 20:41:32 -0600

    dark-mode: Added basic toggle button
    To change theme

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,19 @@
-console.log("js here")
+const asignToggleThemeToButton = (button, main) => {
+    button.addEventListener("click", () => main.classList.toggle("dark-mode"))
+}
+
+(() => {
+    window.addEventListener("load", () => {
+        const MAIN  = document.querySelector(".main-content")
+        const DARK_MODE_TOGGLE_BUTTON = document.querySelector("#toogleDarkModeButton")
+
+        function main() {
+            if(MAIN && DARK_MODE_TOGGLE_BUTTON) {
+                asignToggleThemeToButton(DARK_MODE_TOGGLE_BUTTON, MAIN)
+            }
+        }
+
+        main()
+
+    })
+})()
